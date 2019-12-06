d74304f603585bbde411e671b0a4202401aa86cd
kawa1228 <sxa.1105bo@gmail.com>
2019-12-06 11:33:26 +0900

    updated the app.js component use thi useReducer hookk

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useReducer, useEffect} from 'react';
 import '../App.css';
 import Header from "./Header"
 import Movie from "./Movie"
@@ -6,43 +6,79 @@ import Search from "./Search"
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=e7ea5ca0"
 
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      }
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      }
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      }
+    default:
+      return state
+  }
+}
+
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   // 最初のレンダリング後, 更新ごとに実行
   // Functional ComponentでLifeCycleが扱える（componentDidMount, componentDidUpdate, componentWillUnMountを1つに）
   useEffect(() => {
     fetch(MOVIE_API_URL).then(response => response.json()).then(jsonResponse => {
-      console.log('use effect json', jsonResponse)
-      setMovies(jsonResponse.Search)
-      setLoading(false)
+      dispatch({
+        type: "SEARCH_MOVIES_SUCCESS",
+        payload: jsonResponse.Search
+      })
     })
     // 配列は渡された値が変更されていない場合、エフェクトの適用をスキップ
   }, [])
 
   const search = searchValue => {
-    setLoading(true)
-    setErrorMessage(null)
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    })
 
     fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=e7ea5ca0`)
       .then(response => response.json())
       .then(jsonResponse => {
-        console.log('search json', jsonResponse)
-        if (jsonResponse === "True") {
-          setMovies(jsonResponse.Search)
-          setLoading(false)
+        if (jsonResponse.Response === "True") {
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.Search
+          })
         } else {
-          setErrorMessage(jsonResponse.Error)
-          setLoading(false)
+          dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.Error
+          })
         }
       })
   }
 
+  const {movies, errorMessage, loading} = state
+
   return (
     <div className="App">
-      <Header text="HOOKED" />
+      <Header text="movie" />
       <Search search={search} />
       <p className="App-intro">Sharing a few of our favorite movies</p>
       <div className="movies">
